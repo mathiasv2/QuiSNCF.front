6ba3112e64711284814776a5e19ab4f5939458f0
mathiasv2 <dumas.mathias@outlook.com>
2026-03-22 19:00:33 +0100

    shits i will have to refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useRandomStation, useCheckUserInput } from "./hooks/useRandomStation"
 import { ZoomImage } from "./components/station-image"
 import { TextInput } from "./components/answser-input"
 
-const ZOOM_LEVELS = [400, 300, 200, 150, 120, 100];
+const ZOOM_LEVELS = [1000, 600,550, 500,450, 400, 350, 300, 250, 200, 150, 120, 100];
 
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
   const handleSubmit = async () => {
     if (!(await checkInput(input))) {
       setAttempts((a) => Math.min(a + 1, ZOOM_LEVELS.length - 1));
+      console.log(station.pictureUrl)
     }
     setInput("");
   };
@@ -26,11 +27,12 @@ function App() {
 return (
     <div className="bg-black">
       <h1 className="text-3xl text-white">{station.name}</h1>
-      <p>{station.city}</p>
+      <p className="text-3xl text-white"> {station.city}</p>
       <p>{station.hint}</p>
       <div className="flex flex-col">
         <div className="flex flex-col items-center gap-4 justify-center">
-          <ZoomImage src="/metz.jpg" zoom={ZOOM_LEVELS[attempts]} />
+          <ZoomImage src={`${station.pictureUrl}.jpg`} zoom={ZOOM_LEVELS[attempts]} />
+          
         </div>
         <div className="flex flex-row justify-center pt-2 gap-2">
           <TextInput
