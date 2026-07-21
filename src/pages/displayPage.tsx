6278ceaebd303699a908f9e88a43c1bde7afe576
mathiasv2 <dumas.mathias@outlook.com>
2026-07-21 15:49:59 +0200

    page for displaying like in station

---
+++ src/pages/displayPage.tsx
@@ -0,0 +1,9 @@
+import { StationDisplay } from "../components/stationDisplay";
+
+export function DisplayPage(){
+  return (
+    <div className="flex justify-center px-4 py-6">
+      <StationDisplay></StationDisplay>
+    </div>
+  )
+}
